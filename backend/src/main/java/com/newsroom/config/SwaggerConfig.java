08de3cdc4bae3975eff6caa10c0b890e3d8304d4
Mambo <haochuhuy@gmail.com>
2025-10-03 14:41:34 +0700

    swagger ngay

---
+++ backend/src/main/java/com/newsroom/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.newsroom.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("NewsRoom API")
+                        .description("API Documentation for NewsRoom Backend")
+                        .version("1.0.0"));
+    }
+}
